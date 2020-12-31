3a08a48296d8fac56e2fc8ec60d7aedfb1fa794b
hanshung <76804712+hanshung@users.noreply.github.com>
2020-12-31 15:29:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const FormAutoFill = new Vue({
   data: {
 
     // Google Apps Script 部署為網路應用程式後的 URL
-    gas: 'https://script.google.com/macros/s/AKfycbwyY7oLpoZ4lbcIwIWniLZ8U_pY8e7YBK9fakeszgR_77S2u1SV/exec',
+    gas: 'https://script.google.com/macros/s/AKfycbwv6zhZjgzc2chXUhR7K0rpr7H5Pb3EOoSquv_E_rzF6jz1LyA/exec',
 
     id: '',
 
@@ -14,25 +14,25 @@ const FormAutoFill = new Vue({
     person: {},
 
     // Google Form 的 action URL
-    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeNBnd-yVJ7_-tMq5xaQrvt0j18UtabCFBTM0Eu2O3ivDecuQ/formResponse',
+    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSesKEz45tiaN_sy0n5QHWLeTM1eI_IF8nuuEIR66ON0JIEG_w/formResponse',
     
     // Google Form 各個 input 的 name
     input: {
-      id: 'entry.1815052017',
-      name: 'entry.1543576845',
-      gender: 'entry.892230025',
-      phone: 'entry.1003128242',
-      site: 'entry.260985931',
-      msg: 'entry.1782841550'
+      id: 'entry.235125850',
+      name: 'entry.373777876',
+      pickup: 'entry.2088074152',
+      addr: 'entry.263741919',
+      amount: 'entry.792924016',
+      acc: 'entry.1594533079'
     },
 
     // loading 效果要不要顯示
     loading: false
   },
   methods: {
-    // ID 限填 4 碼
+    // ID 限填 10 碼
     limitIdLen(val) {
-      if(val.length > 4) {
+      if(val.length = 10) {
         return this.id =  this.id.slice(0, 4);
       }
     },
@@ -40,7 +40,7 @@ const FormAutoFill = new Vue({
     submit() {
       // 再一次判斷是不是可以送出資料
       if(this.person.name !== undefined) {
-        let params = `${this.input.id}=${this.person.id}&${this.input.name}=${this.person.name}&${this.input.gender}=${this.person.gender}&${this.input.phone}=${this.person.phone}&${this.input.site}=${this.person.site}&${this.input.msg}=${this.person.message || '無'}`;
+        let params = `${this.input.id}=${this.person.id}&${this.input.name}=${this.person.name}&${this.input.pickup}=${this.person.pickup}&${this.input.addr}=${this.person.addr}&${this.input.amount}=${this.person.amount}&${this.input.msg}=${this.person.msg}`;
         fetch(this.formAction + '?' + params, {
           method: 'POST'
         }).catch(err => {
@@ -54,7 +54,7 @@ const FormAutoFill = new Vue({
   watch: {
     id: function(val) {
       // ID 輸入到 4 碼就查詢資料
-      if(val.length === 4) {
+      if(val.length === 10) {
 
         // this.persons 裡沒這筆資料，才 POST
         if(this.persons[this.id] === undefined) {
